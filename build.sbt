42addc4962d7b1bd97ff212463a4873082caa7df
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-09-17 01:39:05 -0700

    CLI parsing

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.12"
 
+libraryDependencies += "org.rogach" %% "scallop" % "5.0.0"
+
 lazy val root = (project in file("."))
   .settings(
     name := "quadris-scala"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,16 @@
+import org.rogach.scallop._
+
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val bonus: ScallopOption[Boolean] = opt[Boolean](default = Some(false))
+  val textonly: ScallopOption[Boolean] = opt[Boolean](default = Some(false))
+  val seed: ScallopOption[Int] = opt[Int](default = Some(0))
+  val startlevel: ScallopOption[Int] = opt[Int](default = Some(0))
+  val scriptfile: ScallopOption[String] = opt[String](default = Some("sequence.txt"))
+  verify()
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val conf = new Conf(args)
   }
 }
